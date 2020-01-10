e069d0eefc7bf5a6b161bf5a50130e78bf41a62b
aeddakhly <ayoube.eddakhly@gmail.com>
2020-01-10 23:49:50 +0100

    adding a test file for 'set'

---
+++ set_test.go
@@ -0,0 +1,24 @@
+package set
+
+import (
+	"testing"
+)
+                    
+func Test(t *testing.T) {
+	st := New(7, 4)
+	st.Insert(3)
+	if st.Size() != 3 {
+		t.Errorf("Set should contain three elements")
+	}
+	st.Remove(3)
+	if st.Size() != 2 {
+		t.Errorf("Set should contain two elements")
+	}
+
+	if !st.Contains(4) {
+		t.Errorf("Set should contain 4")
+	}
+	if st.Contains(3) {
+		t.Errorf("Set shouldn't contain 3 since it was removed")
+	}
+}
